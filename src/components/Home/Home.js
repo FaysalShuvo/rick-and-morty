324f208c71a735a8169db1b99eb647e582e11d9a
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-12-14 12:41:46 +0600

    folder structer DONE

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-center title-text my-3">
+        Rick & Morty <span className="text-danger">Library!</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
